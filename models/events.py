27ea1214f6c085ae539b6f14757858d77310e061
htpham <htpham3096@gmail.com>
2022-08-29 23:49:52 +0700

    implement crud with sql

--- models/events.py
+++ models/events.py
@@ -1,21 +1,42 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
+from sqlmodel import SQLModel, Field, Column, JSON
 
-class Event(BaseModel):
-    id: int
+
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    tags: List[str] = Field(sa_column=Column(JSON))
     location: str
 
     class Config:
         schema_extra = {
             "example": {
                 "title": "Power with FastAPI",
-                "image":"",
+                "image": "",
                 "description": "Learning FastAPI",
-                "tags": ["python", "fastapi", "book","launch"],
-                "location": "Microsoft Team"
+                "tags": ["python", "fastAPI"],
+                "location": "Microsoft Team",
+            }
+        }
+
+
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "FastAPI stuff",
+                "image": "",
+                "description": "Learning FastAPI",
+                "tags": ["python", "fastAPI"],
+                "location": "Microsoft Team",
             }
         }
--- routes/events.py
+++ routes/events.py
@@ -1,6 +1,16 @@
-from fastapi import APIRouter, Body, HTTPException, status
-from models.events import Event
+from fastapi import (
+    APIRouter,
+    Body,
+    HTTPException,
+    status,
+    Depends,
+    Request,
+    status,
+)
+from database.connection import get_session
+from models.events import Event, EventUpdate
 from typing import List
+from sqlmodel import select
 
 event_router = APIRouter(tags=["Events"])
 
@@ -8,15 +18,18 @@ events = []
 
 
 @event_router.get("/", response_model=List[Event])
-async def retrieve_all_events() -> List[Event]:
+async def retrieve_all_events(session=Depends(get_session)) -> List[Event]:
+    _qr = select(Event)
+    events = session.exec(_qr).all()
     return events
 
 
 @event_router.get("/{id}", response_model=Event)
-async def retrieve_event(id: int) -> Event:
-    for event in events:
-        if event.id == id:
-            return event
+async def retrieve_event(id: int, session=Depends(get_session)) -> Event:
+    event = session.get(Event, id)
+
+    if event:
+        return event
 
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, detail="Event not found"
@@ -24,18 +37,69 @@ async def retrieve_event(id: int) -> Event:
 
 
 @event_router.post("/")
-async def create_event(body: Event = Body(...)) -> dict:
-    events.append(body)
+async def create_event(event: Event, session=Depends(get_session)):
+    session.add(event)
+    try:
+        session.commit()
+    except Exception as e:
+        print(e)
+        session.rollback()
+        return HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Occur error!!!"
+        )
+    finally:
+        session.refresh(event)
+
     return {"message": "Create successfully."}
 
 
+@event_router.put("/{id}", response_model=Event)
+async def update_event(
+    id: int, _update: EventUpdate, session=Depends(get_session)
+):
+    _event = session.get(Event, id)
+    if _event is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Event with id={id} not found",
+        )
+
+    _data = _update.dict(exclude_unset=True)
+    for _k, _v in _data.items():
+        setattr(_event, _k, _v)
+
+    session.add(_event)
+    try:
+        session.commit()
+    except Exception as e:
+        print(e)
+        session.rollback()
+        return HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Occur error!!!"
+        )
+    finally:
+        session.refresh(_event)
+
+    return _event
+
+
 @event_router.delete("/{id}")
-async def delete_event(id: int) -> dict:
-    for event in events:
-        if event.id == id:
-            events.remove(event)
-            return {"message": "Delete successfully!!!"}
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Event with id={id} does not exist.",
-    )
+async def delete_event(id: int, session=Depends(get_session)) -> dict:
+
+    _event = session.get(Event, id)
+
+    if _event is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Event with id={id} not found",
+        )
+
+    session.delete(_event)
+    try:
+        session.commit()
+    except Exception as e:
+        print(e)
+        return HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Occur error!!!"
+        )
+    return {"message": "Delete successfully"}
